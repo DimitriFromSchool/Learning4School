b8d6f0c1f9a03c5e71f52fb3ad8763384b24d068
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-16 14:36:31 +0200

    libft - 2 upper 2 lower

--- 42 reloaded/_Libft_/ft_toupper.c
+++ 42 reloaded/_Libft_/ft_toupper.c
@@ -6,7 +6,7 @@
  *  ──────────────────────────────────────────────────────────────
  *           Project: ft_toupper.c
  *                    | Created: 2024-05-16, 12:00:49 |
- *                    | Updated: 2024-05-16, 13:38:11 | 
+ *                    | Updated: 2024-05-16, 14:35:43 | 
  *  ──────────────────────────────────────────────────────────────
  */
 
@@ -17,4 +17,16 @@ int ft_toupper(int c)
     if (c >= 'a' && c <= 'z')
         c = c - 32;
     return (c);
+}
+
+int main(void)
+{
+    int l;
+    l = 68;
+    
+    if ( l != ft_toupper(l))
+        printf("%c deviens %c",l, ft_toupper(l));
+    else 
+        printf("%c n'est pas une lettre minuscule", l);
+    return (0);
 }
