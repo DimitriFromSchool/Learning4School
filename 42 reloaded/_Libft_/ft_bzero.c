1324e8dbd4b633ec5734a26aa9537689b42d95e4
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-23 15:33:09 +0200

    libft

--- 42 reloaded/_Libft_/ft_bzero.c
+++ 42 reloaded/_Libft_/ft_bzero.c
@@ -6,13 +6,45 @@
   ────────────────────────────────────────────────────────────
            Project: ft_bzero.c
                       Created: 2024-05-23, 13:49:07 |
-                      Updated: 2024-05-23, 14:01:01 | 
+                      Updated: 2024-05-23, 15:31:39 | 
   ────────────────────────────────────────────────────────────
  */
 #include <stdio.h>
 
-void bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
-    int i;
-    unsigned char *buffer;
+    size_t          i;
+    unsigned char   *buffer;
+    
+    i = 0;
+    buffer = (unsigned char *)s;
+
+    while (i < n)
+    {
+        buffer[i] = 0;
+        i++;
+    }
+}
+
+int main(void)
+{
+    #include <strings.h>
+    char input[] = "Hello";
+    char inputII[] = "Hello";
+    size_t length = 6;
+    
+    ft_bzero(input,length);
+    bzero(inputII,length);
+
+    for (size_t i = 0; i < length; i++) 
+    {
+        printf("%02x ", input[i]);
+    }
+    printf("\n");
+
+    for (size_t i = 0; i < length; i++) 
+    {
+        printf("%02x ", inputII[i]);
+    }
+    return (0);
 }
