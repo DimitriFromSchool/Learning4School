8929e0956466b7e5c5f420c1f53fc6ce653730fe
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-23 13:47:42 +0200

    memsetmain

--- 42 reloaded/_Libft_/ft_memset.c
+++ 42 reloaded/_Libft_/ft_memset.c
@@ -6,13 +6,34 @@
   ────────────────────────────────────────────────────────────
            Project: ft_memset.c
                       Created: 2024-05-16, 21:49:35 |
-                      Updated: 2024-05-16, 21:50:52 | 
+                      Updated: 2024-05-23, 13:46:47 | 
   ────────────────────────────────────────────────────────────
  */
-
 #include <stdio.h>
 
-void *memset(void *s, int c, size_t n)
+void *ft_memset(void *s, int c, size_t n)
 {
+    unsigned char *buffer;
+    size_t i;
     
+    buffer = (unsigned char *)s;
+    i = 0;
+    while (i < n)
+    {
+        buffer[i] = c;
+        i++;
+    }
+    return (s);
+}
+
+int main(void)
+{
+    //#include <string.h>
+    char    test[] = "Salut";
+    int     data = 68;
+    size_t  stop = 6;
+    char    *ret = ft_memset(test,data,stop);
+    
+    printf ("%s\n", ret);
+    //printf ("%s", (char*)memset(test,data,stop));
 }
