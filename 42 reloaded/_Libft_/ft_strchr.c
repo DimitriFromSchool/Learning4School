1932de0e820ffaadddb4280bc3a7ec558b9cb7a2
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-26 23:08:46 +0200

    libft

--- 42 reloaded/_Libft_/ft_strchr.c
+++ 42 reloaded/_Libft_/ft_strchr.c
@@ -6,11 +6,12 @@
   ────────────────────────────────────────────────────────────
            Project: ft_strchr.c
                       Created: 2024-05-26, 22:54:02 |
-                      Updated: 2024-05-26, 23:05:36 | 
+                      Updated: 2024-05-26, 23:08:42 | 
   ────────────────────────────────────────────────────────────
  */
 
 #include<stdio.h>
+#include<string.h>
 
 char *ft_strchr(const char *s, int c)
 {
@@ -33,7 +34,9 @@ char *ft_strchr(const char *s, int c)
 int main(void)
 {
     char text[] = "Hello";
+    char text2[] = "Hello";
     int letter = 108;
-    printf("%s",ft_strchr(text, letter));
+    printf("%s\n",ft_strchr(text, letter));
+    printf("%s\n",strchr(text2, letter));
     
 }
