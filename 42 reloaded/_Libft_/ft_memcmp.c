aacdf9dc0d15b5129ae9c92a20248e7213ef968e
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-28 23:07:09 +0200

    libft

---
+++ 42 reloaded/_Libft_/ft_memcmp.c
@@ -0,0 +1,24 @@
+/*
+             ░       ░                             ░    ░   
+    .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+     ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+                                 github:DimitriFromSchool::.
+  ────────────────────────────────────────────────────────────
+           Project: ft_memcmp.c
+                      Created: 2024-05-28, 23:00:21 |
+                      Updated: 2024-05-28, 23:07:00 | 
+  ────────────────────────────────────────────────────────────
+ */
+
+#include<stdio.h>
+
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+    unsigned char *mem1;
+    unsigned char *mem2;
+    size_t i;
+
+    mem1 = (unsigned char *) s1;
+    mem2 = (unsigned char *) s2;
+    
+}
