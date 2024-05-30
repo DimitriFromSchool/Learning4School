64b5a462fba3a991f305ae216697339b710573bd
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-30 23:54:57 +0200

    ex1009 bonus

--- C 4 Dummies/ex1009.c
+++ C 4 Dummies/ex1009.c
@@ -6,22 +6,33 @@
   ────────────────────────────────────────────────────────────
            Project: ex1009.c
                       Created: 2024-05-30, 23:40:25 |
-                      Updated: 2024-05-30, 23:47:32 | 
+                      Updated: 2024-05-30, 23:54:48 | 
   ────────────────────────────────────────────────────────────
  */
 #include<stdio.h>
 
-float convertir(float f);
+float   convertir(float f);
+char    choixConversion;
 
 int main(void)
 {
     float temp_f, temp_c;
     
-    printf("Entrez température en Fahrenheit :");
-    scanf("%f", &temp_f);
-    temp_c = convertir(temp_f);
-    printf("%1.1fF correspond à %1.1fC\n" , temp_f, temp_c);
-    return (0);
+    switch (choixConversion)
+    {
+    case 'A':
+        printf("Entrez température en degrés Fahrenheit :");
+        scanf("%f", &temp_f);
+        temp_c = convertir(temp_f);
+        printf("%1.1fF correspond à %1.1fC\n" , temp_f, temp_c);
+        break;
+    case 'B':
+        printf("Entrez température en degrés Celcius :");
+        scanf("%f", &temp_f);
+        temp_c = convertir(temp_f);
+        printf("%1.1fC correspond à %1.1fF\n" , temp_c, temp_f);
+        break;
+        
 }
 
 float convertir(float f)
