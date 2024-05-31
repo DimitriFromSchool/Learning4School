9b3adad16b038de021dbb63c037dbbeb2c14a382
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-31 15:17:43 +0200

    ex1009 bonus

--- C 4 Dummies/ex1009.c
+++ C 4 Dummies/ex1009.c
@@ -6,48 +6,58 @@
   ────────────────────────────────────────────────────────────
            Project: ex1009.c
                       Created: 2024-05-30, 23:40:25 |
-                      Updated: 2024-05-31, 00:16:42 | 
+                      Updated: 2024-05-31, 15:13:16 | 
   ────────────────────────────────────────────────────────────
  */
 #include<stdio.h>
 
 float   convert2C(float f);
 float   convert2F(float c);
-char    choixConversion;
+void    pickyourChoice(char *choixConversion);
 
 int main(void)
 {
     float temp_f, temp_c;
     char choixConversion;
 
+    choixConversion = 'X';
+    while ( choixConversion != 'A' && choixConversion != 'B')
+    {   
+        pickyourChoice(&choixConversion);
+ 
+        if (choixConversion == 'A')
+        {
+            printf("Entrez température en degrés Fahrenheit :");
+            scanf("%f", &temp_f);
+            temp_c = convert2C(temp_f);
+            printf("%1.1fF correspond à %1.1fC\n" , temp_f, temp_c);
+        }
+        else if (choixConversion == 'B')
+        {
+            printf("Entrez température en degrés Celsius :");
+            scanf("%f", &temp_c);
+            temp_f = convert2F(temp_c);
+            printf("%1.1fC correspond à %1.1fF\n" , temp_c, temp_f);
+        }
+        else
+        {
+            printf("Votre Choix semble incorrect\n");
+            puts("\n");
+        }
+    }
+    return (0);  
+}
+
+void    pickyourChoice(char *choixConversion)
+{
     printf("Quelle conversion souhaitez vous efffectuer ?\n");
     printf(" A - Pour convertir Fahrenheit en Celsius\n");
     printf(" B - Pour convertir Celsius en Fahrenheit\n");
     puts("\n");
     
     printf("Entrez votre choix :");
-    scanf("%c", &choixConversion);
+    scanf(" %c", choixConversion);
     puts("\n");
-    
-    
-    switch (choixConversion)
-    {
-    case 'A':
-        printf("Entrez température en degrés Fahrenheit :");
-        scanf("%f", &temp_f);
-        temp_c = convert2C(temp_f);
-        printf("%1.1fF correspond à %1.1fC\n" , temp_f, temp_c);
-        break;
-    case 'B':
-        printf("Entrez température en degrés Celsius :");
-        scanf("%f", &temp_c);
-        temp_f = convert2F(temp_c);
-        printf("%1.1fC correspond à %1.1fF\n" , temp_c, temp_f);
-        break;
-    default :
-        printf("Votre Choix semble incorrect");
-    }
-    
 }
 
 float convert2C(float f)
