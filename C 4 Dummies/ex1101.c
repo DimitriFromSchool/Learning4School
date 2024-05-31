6b0f81a7fdc88be1d16871012a1a10ca6a38837c
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-31 16:28:43 +0200

    ex1101

--- C 4 Dummies/ex1101.c
+++ C 4 Dummies/ex1101.c
@@ -4,49 +4,20 @@
      ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
                                  github:DimitriFromSchool::.
   ────────────────────────────────────────────────────────────
-           Project: ex1013.c
-                      Created: 2024-05-31, 15:53:00 |
-                      Updated: 2024-05-31, 16:13:29 | 
+           Project: ex1101.c
+                      Created: 2024-05-31, 16:21:39 |
+                      Updated: 2024-05-31, 16:28:29 | 
   ────────────────────────────────────────────────────────────
  */
 
-#include <stdio.h>
-
-void limiter(int stop);
-int verifier(int num);
+#include<stdio.h>
 
 int main(void)
 {
-    int s;
-    
-    printf("Indiquez une valeur entre 0 et 100:");
-    scanf(" %d", &s);
-    if (verifier(s) == 1)
-        limiter(s);
-    return (0);
-}
-
-void limiter(int stop)
-{
     int x;
-    for (x = 0; x <= 100; x = x + 1)
+    for (x = 0; x < 10; x++)
     {
-        printf("%d ", x);
-        if (x == stop)
-        {
-            puts("Vous avez gagné!");
-            return;
-        }       
+        printf("%d - Bonjour\n", x + 1);
     }
-    puts("Je gagne !");
-}
-
-int verifier(int num)
-{
-    if (num < 0 || num > 100)
-    {
-        printf("Perdu ! Vous n'avez pas respecté les règles!");
-        return (0);
-    }
-    return (1);
+    return (0);
 }
