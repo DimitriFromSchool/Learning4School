be9ba224a31206536be8fbcf76fb03e15c76f7e7
DimitriFromSchool <dimitri4school@gmail.com>
2024-05-31 16:05:45 +0200

    ex1013

--- C 4 Dummies/ex1013.c
+++ C 4 Dummies/ex1013.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ex1013.c
                       Created: 2024-05-31, 15:53:00 |
-                      Updated: 2024-05-31, 15:58:41 | 
+                      Updated: 2024-05-31, 16:05:00 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -21,23 +21,21 @@ int main(void)
     
     printf("Indiquez une valeur entre 0 et 100:");
     scanf(" %d", &s);
-    limiter(s);
+    if (verifier(s) == 1)
+        limiter(s);
     return (0);
 }
 
 void limiter(int stop)
 {
     int x;
-    if ( verifier(stop) == 1)
+    for (x = 0; x <= 100; x = x + 1)
     {
-        for (x = 0; x <= 100; x = x + 1)
+        printf("%d ", x);
+        if (x == stop)
         {
-            printf("%d ", x);
-            if (x == stop)
-            {
-                puts("Vous avez gagné!");
-                return;
-            }
+            puts("Vous avez gagné!");
+            return;
         }
     }
     puts("Je gagne !");
@@ -45,7 +43,7 @@ void limiter(int stop)
 
 int verifier(int num)
 {
-    if ( num < 0 && num > 100)
+    if (num < 0 || num > 100)
     {
         printf("Perdu ! Vous n'avez pas respecté les règles!");
         return (0);
