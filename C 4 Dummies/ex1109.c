f5baf3dcb0c69793da7754ac1370f69678a7d640
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-02 22:05:38 +0200

    ex1109

---
+++ C 4 Dummies/ex1109.c
@@ -0,0 +1,30 @@
+/*
+             ░       ░                             ░    ░   
+    .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+     ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+                                 github:DimitriFromSchool::.
+  ────────────────────────────────────────────────────────────
+           Project: ex1109.c
+                      Created: 2024-06-02, 22:01:25 |
+                      Updated: 2024-06-02, 22:05:14 | 
+  ────────────────────────────────────────────────────────────
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    float alpha;
+    
+    alpha = 501;
+    printf("alpha = %1.1f\n", alpha);
+    alpha = alpha + 99;
+    printf("alpha = %1.1f\n", alpha);
+    alpha = alpha - 250;
+    printf("alpha = %1.1f\n", alpha);
+    alpha = alpha / 82;
+    printf("alpha = %1.1f\n", alpha);
+    alpha = alpha * 4.3;
+    printf("alpha = %1.1f\n", alpha);
+    return (0);
+}
