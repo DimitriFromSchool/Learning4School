502945cbeb68a58189e00c70074270551d1ecafd
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-03 23:43:58 +0200

    ex1111

--- C 4 Dummies/ex1111.c
+++ C 4 Dummies/ex1111.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ex1111.c
                       Created: 2024-06-03, 23:20:53 |
-                      Updated: 2024-06-03, 23:34:48 | 
+                      Updated: 2024-06-03, 23:43:54 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -19,7 +19,14 @@ int main(void)
     
     printf("Indiquez une valeur factionnaire :");
     scanf("%f", &valeur);
+    
     resultat = sqrt(valeur);
     printf("%.2f Racine carrée de = %.2f\n", valeur, resultat);
+    resultat = pow(valeur, 3);
+    printf("%.2f Puissance cubique de = %.2f\n", valeur, resultat);
+    resultat = floor(valeur);
+    printf("Arondi par défaut de %.2f = %.2f\n", valeur, resultat);
+    resultat = ceil(valeur);
+    printf("Arondi par exces de %.2f = %.2f\n", valeur, resultat);
     return (0);
 }
