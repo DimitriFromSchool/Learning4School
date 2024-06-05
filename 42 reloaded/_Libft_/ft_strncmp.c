2737b459d3c60f2eafdc73b3be5ec38058b97ffb
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-05 12:56:49 +0200

    strncmp

--- 42 reloaded/_Libft_/ft_strncmp.c
+++ 42 reloaded/_Libft_/ft_strncmp.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ft_strncmp.c
                       Created: 2024-06-04, 21:52:52 |
-                      Updated: 2024-06-05, 08:45:40 | 
+                      Updated: 2024-06-05, 12:56:28 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -22,19 +22,22 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     str1 = (unsigned char *) s1;
     str2 = (unsigned char *) s2;
     i = 0;
-    while (str1[i] != '\0' && str1[i] != '\0' && i < n)
+    while (i < n)
     {
+        if (str1[i] != '\0' || str1[i] != '\0' )
+            return (str1[i]-str2[i]);
         i++;
     }
-    return (str1[i]-str2[i]);
+    return (0);
+   
 }
 
 int main(void)
 {
-    char *s1 = "Coucou";
+    char *s1 = NULL;
     char *s2 = NULL;
     size_t n = 0;
     printf("ft_strncmp : %d \n", ft_strncmp(s1,s2,n));
-    //printf("strncmp : %d \n", strncmp(s1,s2,n));
+    printf("strncmp : %d \n", strncmp(s1,s2,n));
     return (0);
 }
