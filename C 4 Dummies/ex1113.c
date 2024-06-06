ad9450ad6af81c967dfab7c87dde5566fa3703cb
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-06 23:43:20 +0200

    ex1113

---
+++ C 4 Dummies/ex1113.c
@@ -0,0 +1,24 @@
+/*
+ *             ░       ░                             ░    ░   
+ *    .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+ *     ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+ *                                 github:DimitriFromSchool::.
+ *  ────────────────────────────────────────────────────────────
+ *           Project: ex1113
+ *                      Created: 2024-06-06, 23:38:55 |
+ *                      Updated: 2024-06-06, 23:38:55 | 
+ *  ────────────────────────────────────────────────────────────
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    float degres,radians;
+
+    printf("Indiquez un angle en degrés : ");
+    scanf("%f", &degres);
+    radians = 0.0174532925*degres;
+    printf("%1.2f degres valent %1.2f radians.\n", degres, radians);
+    return (0);
+}
