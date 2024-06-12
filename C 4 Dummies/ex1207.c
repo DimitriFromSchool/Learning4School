478dd5fa785fd4ba692ce216b9b59f2d5801288f
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-12 23:59:08 +0200

    ex1207.c

--- C 4 Dummies/ex1207.c
+++ C 4 Dummies/ex1207.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ex1207.c
                       Created: 2024-06-12, 23:33:36 |
-                      Updated: 2024-06-12, 23:56:35 | 
+                      Updated: 2024-06-12, 23:59:00 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -19,9 +19,11 @@ int main (void)
     int base[] = {10, 12, 14, 15, 16, 18, 20};
     int racine[7];
     int i;
+    int tabsize;
 
     i = 0;
-    while ( i < 7 )
+    tabsize = sizeof(base) / sizeof(base[0]);
+    while ( i < tabsize )
     {
         racine[i] = (int)sqrt(base[i]);
         printf("%d \t", base[i]);
