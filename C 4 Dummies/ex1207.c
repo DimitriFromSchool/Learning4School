f5cce50a9ad35ff352d60c3548189aebc7a737f1
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-13 00:01:30 +0200

    ex1207.c

--- C 4 Dummies/ex1207.c
+++ C 4 Dummies/ex1207.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ex1207.c
                       Created: 2024-06-12, 23:33:36 |
-                      Updated: 2024-06-12, 23:59:00 | 
+                      Updated: 2024-06-13, 00:01:24 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -22,9 +22,9 @@ int main (void)
     int tabsize;
 
     i = 0;
-    tabsize = sizeof(base) / sizeof(base[0]);
-    while ( i < tabsize )
-    {
+    tabsize = sizeof(base) / sizeof(base[0]);  // pour obtenir le nombre d'éléments, on divise la taille totale du tableau par la taille d'un élément
+    while ( i < tabsize ) // les tableau de int ne se terminent pas par un charactère nul comme 
+    {                     //pour les chaines de caractères
         racine[i] = (int)sqrt(base[i]);
         printf("%d \t", base[i]);
         printf("%d \n", racine[i]);
