f226921fcc729a07a5fd3b1cff3c2ab342b0d2fa
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-16 23:15:14 +0200

    wait and see...

--- C 4 Dummies/ex1209.c
+++ C 4 Dummies/ex1209.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ex1209.c
                       Created: 2024-06-13, 23:36:43 |
-                      Updated: 2024-06-13, 23:37:27 | 
+                      Updated: 2024-06-16, 23:15:01 | 
   ────────────────────────────────────────────────────────────
  */
 
@@ -26,3 +26,5 @@ int main (void)
     putchar('\n');
     return (0);
 }
+
+//After a little weekend in paris, back to the basics . Wait and see...
