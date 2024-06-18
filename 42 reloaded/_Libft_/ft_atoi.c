9f9e23196f83de221c9db9ddb115849137287651
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-18 21:56:24 +0200

    work in progress

--- 42 reloaded/_Libft_/ft_atoi.c
+++ 42 reloaded/_Libft_/ft_atoi.c
@@ -6,21 +6,35 @@
   ────────────────────────────────────────────────────────────
            Project: ft_atoi.c
                       Created: 2024-06-05, 12:58:25 |
-                      Updated: 2024-06-09, 23:03:16 | 
+                      Updated: 2024-06-18, 21:55:53 | 
   ────────────────────────────────────────────────────────────
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/*int ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
+    int i;
+    int num;
+    i = 0;
+
+    while (str[i] != '\0')
+    {
+        //if ( str[i] == '+' || str[i] == '-' )
+        if ( str[i] < 48 && str[i] > 57 )
+            return (1);
+        num = str[i] - 48;
+        
+    }
+    
+    
     return (0);
-}*/
+}
 
 int main(void)
 {
-    char *ascii = "2247483647";
+    char *ascii = "A-00000483647";
     int result = atoi(ascii) + 1;
 
     printf("%d \n", result);
