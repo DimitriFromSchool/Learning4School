bfe7079249e48176c660c41ac109dd8a3459f8c1
DimitriFromSchool <dimitri4school@gmail.com>
2024-06-22 23:54:33 +0200

    1213.c

---
+++ C 4 Dummies/ex1213.c
@@ -0,0 +1,25 @@
+/*
+             ░       ░                             ░    ░   
+    .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+     ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+                                 github:DimitriFromSchool::.
+  ────────────────────────────────────────────────────────────
+           Project: ex1213.c
+                      Created: 2024-06-22, 23:47:58 |
+                      Updated: 2024-06-22, 23:54:02 | 
+  ────────────────────────────────────────────────────────────
+ */
+
+#include <stdio.h> 
+
+#define TAILLE 6
+
+int main()
+{
+    int tabulles[] = { 95, 60, 6, 87, 50, 24 };
+    int interne, externe, temp, x;
+    
+    /*Affiche le tableau initial */
+    puts("Tableau de départ : ");
+    
+}
