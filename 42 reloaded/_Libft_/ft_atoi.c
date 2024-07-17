a54a0f9f0971d99c356e7cf6ba0b0818765aed87
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-17 13:47:33 +0200

    ft_atoi.c

--- 42 reloaded/_Libft_/ft_atoi.c
+++ 42 reloaded/_Libft_/ft_atoi.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ft_atoi.c
                       Created: 2024-06-05, 12:58:25 |
-                    Updated: 2024-07-17, 1:26:19 |
+                    Updated: 2024-07-17, 1:42:47 |
   ────────────────────────────────────────────────────────────
  */
 
@@ -23,19 +23,18 @@ int ft_atoi(const char *str)
     result = 0;
     sign = 1;
 
-    while (str[i] != '\0')
+    while ( str[i] == '\n' || str[i] == '\t' || str[i] == '\f' || str[i] == '\r' || str[i] == '\v' || str[i] == ' ')
+
+    if ( str[i] == '+' || str[i] == '-' )
     {
-        if ( str[i] == '+' || str[i] == '-' )
-        {
-            if (str[i] == '-')
-                sign = -1;
-            i++;
-        }
-        if ( str[i] => 48 || str[i] <= 57)
-        {
-            result = result * 10 + str[i] - 48 
-            i++;
-        }
+        if (str[i] == '-')
+            sign = -1;
+        i++;
+    }
+    while ( str[i] => 48 || str[i] <= 57)
+    {
+        result = result * 10 + str[i] - 48 
+        i++;
     }
     return (result * sign);
 }
