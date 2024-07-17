344949bab957c16752f6c0441ddb88665a615922
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-17 13:26:42 +0200

    ft_atoi.c

--- 42 reloaded/_Libft_/ft_atoi.c
+++ 42 reloaded/_Libft_/ft_atoi.c
@@ -6,7 +6,7 @@
   ────────────────────────────────────────────────────────────
            Project: ft_atoi.c
                       Created: 2024-06-05, 12:58:25 |
-                      Updated: 2024-06-25, 18:07:09 | 
+                    Updated: 2024-07-17, 1:26:19 |
   ────────────────────────────────────────────────────────────
  */
 
@@ -16,25 +16,28 @@
 int ft_atoi(const char *str)
 {
     int i;
-    int num;
     int result;
+    int sign;
+    
     i = 0;
-    num = 0;
+    result = 0;
+    sign = 1;
 
     while (str[i] != '\0')
     {
         if ( str[i] == '+' || str[i] == '-' )
         {
-            num += i;
+            if (str[i] == '-')
+                sign = -1;
             i++;
         }
         if ( str[i] => 48 || str[i] <= 57)
         {
-            result = str[i] - 48 // Créer dizaine;
+            result = result * 10 + str[i] - 48 
             i++;
         }
     }
-    return (result);
+    return (result * sign);
 }
 
 int main(void)
