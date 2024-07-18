409f5e7264008db36d1eaf64b1d43fb204b4f54a
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-18 16:00:19 +0200

    ft_strlcpy.c

--- 42 reloaded/_Libft_/ft_strlcpy.c
+++ 42 reloaded/_Libft_/ft_strlcpy.c
@@ -6,13 +6,31 @@
 ────────────────────────────────────────────────────────────
          Project: ft_strlcpy.c
                     Created: 2024-07-17, 2:06:08 |
-                    Updated: 2024-07-17, 2:18:35 |
+                    Updated: 2024-07-18, 4:00:07 |
 ────────────────────────────────────────────────────────────
 */
 
 #include <stdio.h>
 
-size_t strlcpy(char *dst, const char *src, size_t dstsize);
+size_t strlcpy(char *dst, const char *src, size_t size)
+{
+  size_t i;
+  size_t length;
+  
+  i = 0;
+  length = 0;
+  
+  if (size == 0)
+    return (length);
+    
+  while ( src[i] =! '\0' && i < size )
+  {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
+  return (length);
+}
 
 int main (void)
 {
