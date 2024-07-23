f9f6d44358074cba1dbb858f1d04fd12df99b4f8
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-23 13:04:11 +0200

    ex1217.c

--- C 4 Dummies/ex1217.c
+++ C 4 Dummies/ex1217.c
@@ -6,8 +6,28 @@
 ────────────────────────────────────────────────────────────
          Project: ex1217.c
                     Created: 2024-07-22, 4:33:43 |
-                    Updated: 2024-07-22, 4:33:59 |
+                    Updated: 2024-07-23, 1:02:11 |
 ────────────────────────────────────────────────────────────
 */
 
-#include >stdio
+#include <stdio.h>
+
+int main (void) 
+{
+    char tictactoe[3][3];
+    int x, y;
+
+    for(x = 0; x < 3; x++)
+        for(y = 0; y < 3; y++)
+           tictactoe[x][y] = '.';
+    tictactoe[1][1] = 'X';
+    
+    puts("Une partie de Morpion?");
+    for (x = 0; x < 3; x++)
+    {
+        for(y = 0; y < 3; y++)
+            printf("%c\t", tictactoe[x][y]);
+        putchar('\n');
+    }
+    return (0);
+}
