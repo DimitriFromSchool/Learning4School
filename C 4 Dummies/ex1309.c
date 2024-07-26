c8af3665568a3ddd27c250fdb8d26cd4aaf1b70a
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-26 13:54:45 +0200

    ex1309.c

---
+++ C 4 Dummies/ex1309.c
@@ -0,0 +1,30 @@
+/*
+  ░       ░                             ░    ░   
+  .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+   ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+                               github:DimitriFromSchool::.
+────────────────────────────────────────────────────────────
+         Project: ex1309.c
+                    Created: 2024-07-26, 1:52:32 |
+                    Updated: 2024-07-26, 1:52:42 |
+────────────────────────────────────────────────────────────
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char password[] = "taco";
+    char tabsinput[15];
+
+    printf("Le mot de passe ? ");
+    scanf("%s", tabsinput);
+
+    if ((strcmp(tabsinput, password)) == 0)
+        puts("Mot de passe correct");
+    else
+        puts("Mot de passe incorrect.");
+
+    return (0);
+}
