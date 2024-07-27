9631665e9f10a76dd5bff768a1bb47025d852279
DimitriFromSchool <dimitri4school@gmail.com>
2024-07-27 22:16:32 +0200

    ex1315.c

---
+++ C 4 Dummies/ex1315.c
@@ -0,0 +1,24 @@
+/*
+  ░       ░                             ░    ░   
+  .*✶░█▀▄ █ █▀▄▀█ █ ▀█▀ █▀█ █ █░█ █▀ █▀▀ █░█ █▀█ █▀█ █░✶*..
+   ..*█▄▀ █ █░▀░█ █ ░█░ █▀▄ █ ▀▀█ ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄*✶.
+                               github:DimitriFromSchool::.
+────────────────────────────────────────────────────────────
+         Project: ex1315.c
+                    Created: 2024-07-27, 10:13:18 |
+                    Updated: 2024-07-27, 10:15:30 |
+────────────────────────────────────────────────────────────
+*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    printf("%-9smoi\n", "parle-");
+    printf("%-8smoi\n", "parle-");
+    printf("%-7smoi\n", "parle-");
+    printf("%-6smoi\n", "parle-");
+    printf("%-5smoi\n", "parle-");
+    printf("%-4smoi\n", "parle-");
+    return (0);
+}
